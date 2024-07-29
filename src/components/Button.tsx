39715141f431c558f173c71da10125e6a40e5285
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-29 19:51:43 +0200

    Tried to implement buttons logic on my own

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,80 @@
+import { useContext, useState } from "react"
+import { ScreenContextProvider } from "../contexts/ScreenContext"
+
 interface ButtonProps {
     className?: string
     value: string | number
+    operation?: "reset" | "add" | "minus" | "multiply" | "divide" | "modulus" | "dot" | "equal" | "negate"
 }
 
-export default function Button({ className, value }: ButtonProps) {
+export default function Button({ className, value, operation }: ButtonProps) {
+    const [operand, setOperand] = useState<string>("")
+    const context = useContext(ScreenContextProvider)
 
+    function handleClick() {
+        if (typeof value === "number") {
+            console.log("it's a number");
+            if (context?.screenValue === 0) {
+                context.setScreenValue(value)
+                return
+            } else {
+                if (context?.screenValue.toString().length === 9) {
+                    return
+                }
+                context?.setScreenValue(prevValue => {
+                    return parseInt(prevValue.toString() + value)
+                })
+            }
+        } else {
+            if (context?.screenValue === 0) {
+                return
+            }
+            switch (operation) {
+                case "reset":
+                    context?.setScreenValue(0)
+                    break
+                case "add":
+                    setOperand(context?.screenValue.toString() + "+")
+                    console.log(operand);
+                    break
+                case "minus":
+                    setOperand(context?.screenValue.toString() + "-")
+                    console.log(operand);
+                    break
+                case "divide":
+                    setOperand(context?.screenValue.toString() + "/")
+                    console.log(operand);
+                    break
+                case "multiply":
+                    setOperand(context?.screenValue.toString() + "*")
+                    break
+                case "modulus":
+                    setOperand(context?.screenValue.toString() + "%")
+                    break
+                case "negate":
+                    context?.setScreenValue(prevValue => {
+                        return prevValue * -1
+                    })
+                    break
+                case "dot":
+                    if (context?.screenValue.toString().includes('.')) {
+                        return
+                    }
+                    context?.setScreenValue(prevValue => {
+                        return parseInt(prevValue.toString() + value + 0)
+                    })
+                    break
+                // case "equal":
+                //     setOperand(context?.screenValue as number)
+                //     break
+            }
+        }
+    }
 
     return (
         <button
             className={`bg-gray-300 text-[2rem] border-none outline-none h-28 flex items-center justify-center ${className}`}
+            onClick={handleClick}
         >
             {value}
         </button>
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -7,25 +7,25 @@ export default function Keyboard() {
         <div
             className="w-full grid grid-cols-4 gap-[1px] bg-black"
         >
-            <Button className="" value={"AC"} />
-            <Button className="" value={"+/-"} />
-            <Button className="" value={"%"} />
-            <Button className="bg-orange-500 text-white" value={"÷"} />
+            <Button className="" value={"AC"} operation="reset" />
+            <Button className="" value={"+/-"} operation="negate" />
+            <Button className="" value={"%"} operation="modulus" />
+            <Button className="bg-orange-500 text-white" value={"÷"} operation="divide" />
             <Button className="" value={7} />
             <Button className="" value={8} />
             <Button className="" value={9} />
-            <Button className="bg-orange-500 text-white" value={"x"} />
+            <Button className="bg-orange-500 text-white" value={"x"} operation="multiply" />
             <Button className="" value={6} />
             <Button className="" value={5} />
             <Button className="" value={4} />
-            <Button className="bg-orange-500 text-white" value={"-"} />
+            <Button className="bg-orange-500 text-white" value={"-"} operation="minus" />
             <Button className="" value={3} />
             <Button className="" value={2} />
             <Button className="" value={1} />
-            <Button className="bg-orange-500 text-white" value={"+"} />
+            <Button className="bg-orange-500 text-white" value={"+"} operation="add" />
             <Button className="col-span-2" value={0} />
-            <Button className="" value={"."} />
-            <Button className="bg-orange-500 text-white" value={"="} />
+            <Button className="" value={"."} operation="dot" />
+            <Button className="bg-orange-500 text-white" value={"="} operation="equal" />
         </div>
     );
 }
